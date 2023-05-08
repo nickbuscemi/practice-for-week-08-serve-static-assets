07279d19dd63b4f1c73d6c79ea234627fdcf5240
Nick <nick.buscemi13@gmail.com>
2023-05-08 12:37:24 -0400

    finished

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+  const dogImage = fs.readFileSync('assets/images/dog.jpg');
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'image/png');
+  res.end(dogImage);
 });
 
 const port = 5000;
