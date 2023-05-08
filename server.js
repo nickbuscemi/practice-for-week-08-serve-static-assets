c323362fe280219fdb000ae8d1b4746412a4a990
Nick <nick.buscemi13@gmail.com>
2023-05-08 14:35:28 -0400

    fininshed part2

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 const http = require('http');
 const fs = require("fs");
 
+
 const server = http.createServer((req, res) => {
   // Your code here
-  const dogImage = fs.readFileSync('assets/images/dog.jpg');
+  if (req.url.startsWith('/static')) {
+    
+    const routeParts = req.url.split('/');
+    routeParts.splice(1, 1); // Remove 'static' from the array
+    const filePath = './assets/' + routeParts.slice(1).join('/');
+    const fileExtension = routeParts[routeParts.length - 1].split('.')[1];
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'image/png');
-  res.end(dogImage);
+    const mimeTypeMap = {
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+      'jpg': 'image/jpeg',
+      'jpeg': 'image/jpeg',
+      'png': 'image/png'
+    };
+
+    const mimeType = mimeTypeMap[fileExtension] || 'text/plain';
+
+    fs.readFile(filePath, (err, data) => {
+      if(err) {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('404 Not Found');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', mimeType);
+        res.end(data);
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Invalid URL')
+  }
 });
 
 const port = 5000;
